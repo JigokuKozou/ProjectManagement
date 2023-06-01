943ee1763ea11fd0e8224ed5fa4d4b697496bcee
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-06-01 20:10:55 +0400

    Added no argument constructor and with Employee

--- dto/src/main/java/ru/shchelkin/project_management/dto/response/employee/EmployeeCardDto.java
+++ dto/src/main/java/ru/shchelkin/project_management/dto/response/employee/EmployeeCardDto.java
@@ -1,11 +1,16 @@
 package ru.shchelkin.project_management.dto.response.employee;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import ru.shchelkin.project_management.commons.status.EmployeeStatus;
+import ru.shchelkin.project_management.model.Employee;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@ToString
 public class EmployeeCardDto {
     private Long id;
 
@@ -24,4 +29,16 @@ public class EmployeeCardDto {
     private String login;
 
     private String password;
+
+    public EmployeeCardDto(Employee employee) {
+        id = employee.getId();
+        surname = employee.getSurname();
+        name = employee.getName();
+        patronymic = employee.getPatronymic();
+        jobTitle = employee.getJobTitle();
+        email = employee.getEmail();
+        status = employee.getStatus();
+        login = employee.getLogin();
+        password = employee.getPassword();
+    }
 }
