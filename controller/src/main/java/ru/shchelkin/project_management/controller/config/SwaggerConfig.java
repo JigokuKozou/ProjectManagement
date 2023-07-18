ba06bee189b1a65459b0c97e0c62891bdb6c164a
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-07-19 03:52:50 +0400

    Moved SwaggerConfig in controller module

--- controller/src/main/java/ru/shchelkin/project_management/controller/config/SwaggerConfig.java
+++ controller/src/main/java/ru/shchelkin/project_management/controller/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package ru.shchelkin.project_management.app.config;
+package ru.shchelkin.project_management.controller.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
